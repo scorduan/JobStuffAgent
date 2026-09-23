d9f3e14192eb773e56616c559002647e3abdeb54
Seth Corduan <seth@corduan.net>
2026-09-23 16:56:40 -0400

    Initial commit with boilerplate

---
+++ src/main/java/org/example/jobstuffagent/JobStuffAgentApplication.java
@@ -0,0 +1,13 @@
+package org.example.jobstuffagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobStuffAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobStuffAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jobstuffagent/JobStuffAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jobstuffagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobStuffAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
